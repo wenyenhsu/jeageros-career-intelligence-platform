8c7bbfa7c8ca9712f8dcdc9f56c7f21009c6f7ea
drhanko <32009460+drhanko@users.noreply.github.com>
2026-06-07 18:11:17 -0700

    application show error fix

--- src/apps/applications/forms.py
+++ src/apps/applications/forms.py
@@ -6,3 +6,20 @@ class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
         fields = ['user', 'job_post', 'status', 'applied_at', 'priority', 'referral']
+        widgets = {
+            'user': forms.Select(attrs={'class': 'form-select'}),
+            'job_post': forms.Select(attrs={'class': 'form-select'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+            'applied_at': forms.DateTimeInput(
+                attrs={'type': 'datetime-local', 'class': 'form-control'},
+                format='%Y-%m-%dT%H:%M'
+            ),
+            'priority': forms.NumberInput(attrs={'class': 'form-control'}),
+            'referral': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.applied_at:
+            self.initial['applied_at'] = self.instance.applied_at.strftime('%Y-%m-%dT%H:%M')
